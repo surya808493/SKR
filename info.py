ba1c84bb354136569c568bcd492a22d79e896985
Business00p <businesskaro0565@gmail.com>
2024-11-13 16:21:57 +0530

    Update info.py
    Hh

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
-CHANNEL      = "RahulReviewsYT"
+API_ID       = 27372121
+API_HASH     = "0b368e65ed8b23e5c2c6501b19c95cf2"
+BOT_TOKEN    = "7282682747:AAGJhYlDdQ4q0Cm9m2UYih6lRbh79po92C4"
+SESSION      = "BQDMOQ4ANnLe5nJVBFKpe1uer2OCEL4xR88mWeUjtvCMue8l98xf7HkNUw3FJA8qwVQTcAUTLK8m9faU_xp_fmMBWPBZq0q0PiF-Z-iNU9vXAwaMUfAzpiemDObngvXhogVX2b3NzFng42gMZZyfg_a7ijsmNdvkhweNX1wlrhWXxyJlTQauaHfMSlVnTf-5jkC5zZbz64fKgNRx6KrWcpcpHMiarN8Kvs-UB_D0-Xd1OsR7Pl3hFL3lYKZgs3M3TzSL3-wAjuzcJXoCSn6kEU0Lg0_cuPF_Ym5bvhR_pjBqbCkPEJWvxd4AnNQoAA5gRF68yTMBFuaRWKwkNPV1MVDNwHq2bgAAAAGdZqLwAA"
+DATABASE_URI = "mongodb+srv://sagarbhakti147:sagarbhakti12@database05.kc1du.mongodb.net/?retryWrites=true&w=majority&appName=Database05"
+LOG_CHANNEL  = -1002266854258
+ADMIN        = 6935716592
+CHANNEL      = "publicmovieadda01"
